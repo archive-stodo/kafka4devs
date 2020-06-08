0c4e1848e52b1ae49a08fb1e91e606f3d6beefae
dawid <stodoo@gmail.com>
2020-06-08 23:39:48 +0200

    ProducerRecord, Header, LibraryEventType

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -2,6 +2,7 @@ package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
 import com.learnkafka.producer.LibraryEventProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
 @Slf4j
 @RestController
 public class LibraryEventsController {
@@ -20,13 +24,15 @@ public class LibraryEventsController {
     LibraryEventProducer producer;
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, InterruptedException, ExecutionException, TimeoutException {
         // invoke kafka producer
 
         log.info("before sendLibraryEvent");
 //        producer.sendLibraryEvent(libraryEvent);
-        SendResult<Integer, String> sendResult = producer.sendLibraryEventSynchronous(libraryEvent);
-        log.info("sendResult: {}", sendResult);
+//        SendResult<Integer, String> sendResult = producer.sendLibraryEventSynchronous(libraryEvent);
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
+        producer.sendLibraryEvent_Approach2(libraryEvent);
+
         log.info("after sendLibraryEvent");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
